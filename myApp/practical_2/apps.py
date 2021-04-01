aa5e5bd1af5a1be83dec5c576121f701f390fcc5
root <root@localhost.localdomain>
2021-04-01 12:43:28 +0530

    Practical 1

---
+++ myApp/practical_2/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class Practical2Config(AppConfig):
+    name = 'practical_2'
---
+++ myApp/practical_2/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("practical_no.2  <br> batch:t4 <br> Team Member: <br> 1942207 : Vaibhav Bharat Sontakke <br> 1841059 : Shubham Yadav")

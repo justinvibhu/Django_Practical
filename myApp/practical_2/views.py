4a65728a85d2a4f63d6672775a7744b12a1159cc
vaibhav sontakke <60538947+justinvibhu@users.noreply.github.com>
2021-04-01 13:58:45 +0530

    Update views.py

--- myApp/practical_2/views.py
+++ myApp/practical_2/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("practical_no.2  <br> batch:t4 <br> Team Member: <br> 1942207 : Vaibhav Bharat Sontakke <br> 1841059 : Shubham Yadav")
+    return HttpResponse("<b>Hello World</b><hr><br>practical_no.2  <br> batch:t4 <br> Team Member: <br> 1942207 : Vaibhav Bharat Sontakke <br> 1841059 : Shubham Yadav <br> !942104: Aniket Brahmankar ")
